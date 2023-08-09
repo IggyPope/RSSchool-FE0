2371460e6810c39273e34da3ded19a8895ebe5f6
IggyPope <altko4egar@gmail.com>
2023-08-09 23:58:01 +0300

    feat: add functionality to the burger-btn

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -1,3 +1,11 @@
 console.log(
   'Вёрстка валидная 10/10\nВёрстка семантическая 16/16\nВёрстка соответствует макету 54/54\nОбщие требования к верстке 20/20\nИтого: 100/100'
 );
+
+const burgerButton = document.querySelector('#header__btn-burger');
+const navMenu = document.querySelector('#header__nav-menu');
+
+burgerButton.addEventListener('click', () => {
+  navMenu.classList.toggle('header__nav-menu_open');
+  burgerButton.classList.toggle('header__btn-burger_open');
+});
