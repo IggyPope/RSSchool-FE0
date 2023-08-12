263c70afa63f7284fb4809891eef6f3f1a825b74
IggyPope <altko4egar@gmail.com>
2023-08-12 15:48:18 +0300

    feat: add function to close burger menu on a click outside of the menu

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -1,7 +1,24 @@
 const burgerButton = document.querySelector('#header__btn-burger');
 const navMenu = document.querySelector('#header__nav-menu');
+let isNavMenuOpen = false;
 
-burgerButton.addEventListener('click', () => {
+const closeMenu = (event) => {
+  if (event.target !== navMenu) {
+    toggleMenu(event);
+  }
+};
+
+const toggleMenu = (event) => {
+  isNavMenuOpen = !isNavMenuOpen;
+
   navMenu.classList.toggle('header__nav-menu_open');
   burgerButton.classList.toggle('header__btn-burger_open');
-});
+
+  isNavMenuOpen
+    ? document.addEventListener('click', closeMenu)
+    : document.removeEventListener('click', closeMenu);
+
+  event.stopPropagation();
+};
+
+burgerButton.addEventListener('click', toggleMenu);
