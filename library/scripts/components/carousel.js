41618c623f7a2a0fcc95307f038f27948b6aa00c
IggyPope <altko4egar@gmail.com>
2023-09-04 19:38:40 +0300

    fix: implement carousel position recalculation on container resize to prevent carousel sliding out of boundaries

--- library/scripts/components/carousel.js
+++ library/scripts/components/carousel.js
@@ -1,14 +1,20 @@
 let position = 0;
 let posIndex = 0;
+const itemsToScroll = 1;
+
+const container = document.querySelector('.carousel__container');
+let containerWidth = container.clientWidth;
+
 const items = document.querySelectorAll('.carousel__item');
 const itemsCount = items.length;
 const itemWidth = document.querySelector('.carousel__item').clientWidth;
-const container = document.querySelector('.carousel__container');
-const containerWidth = container.clientWidth;
-const itemsToScroll = 1;
-const itemsToShow = containerWidth === 1400 ? 3 : 1;
+
+let itemsToShow = containerWidth === 1400 ? 3 : 1;
+let maxPosIndex = itemsCount - itemsToShow;
+
 const track = document.querySelector('.carousel__track');
 const gap = track.computedStyleMap().get('column-gap').value;
+
 const btnPrev = document.querySelector('#carouselButtonPrev');
 const btnNext = document.querySelector('#carouselButtonNext');
 const bullets = document.querySelectorAll('.carousel-pagination__button');
@@ -21,6 +27,16 @@ export default function initializeCarousel() {
   bullets.forEach((bullet, index) => {
     bullet.addEventListener('click', () => handleBulletClick(bullet, index));
   });
+
+  const containerResizeObserver = new ResizeObserver((entries) => {
+    entries.forEach((entry) => {
+      calculateItemsToShow();
+      calculateMaxPosIndex();
+      setTrackPosition();
+    });
+  });
+
+  containerResizeObserver.observe(container);
 
   checkButtons();
 }
@@ -46,9 +62,14 @@ function handleBulletClick(bullet, bulletIndex) {
 }
 
 function setTrackPosition() {
-  track.style.transform = `translateX(-${position}px)`;
-
   posIndex = Math.round(position / (itemWidth + gap));
+
+  if (posIndex > maxPosIndex) {
+    posIndex = maxPosIndex;
+    position = posIndex * (itemWidth + gap);
+  }
+
+  track.style.transform = `translateX(-${position}px)`;
 
   checkButtons();
 }
@@ -63,4 +84,13 @@ function checkButtons() {
   bullets.forEach((bullet, index) => {
     bullet.ariaCurrent = index === posIndex;
   });
+}
+
+function calculateItemsToShow() {
+  containerWidth = container.clientWidth;
+  itemsToShow = containerWidth === 1400 ? 3 : 1;
+}
+
+function calculateMaxPosIndex() {
+  maxPosIndex = itemsCount - itemsToShow;
 }
